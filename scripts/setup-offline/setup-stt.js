41411a9ab9c083f2060170d8da4aa26043ed270a
ArminasTV <arminas.stream@gmail.com>
2020-05-10 18:11:27 +0200

    fix(server): jdkldjflkgd

--- scripts/setup-offline/setup-stt.js
+++ scripts/setup-offline/setup-stt.js
@@ -12,7 +12,7 @@ export default () => new Promise(async (resolve, reject) => {
 
   const destDeepSpeechFolder = 'bin/deepspeech'
   const tmpDir = 'scripts/tmp'
-  const deepSpeechVersion = '0.5.0'
+  // const deepSpeechVersion = '0.5.0'
   // const archiveName = `deepspeech-${deepSpeechVersion}-models.tar.gz`
   const archiveName = 'model_tflite_fr.tar.xz'
   let downloader = 'wget'
@@ -22,16 +22,15 @@ export default () => new Promise(async (resolve, reject) => {
 
   if (!fs.existsSync(`${destDeepSpeechFolder}/lm.binary`)) {
     try {
-      log.info('Downloading pre-trained model...')
-      // await shell(`cd ${tmpDir} && ${downloader} https://github.com/mozilla/DeepSpeech/releases/download/v${deepSpeechVersion}/${archiveName}`)
+      log.info('Downloading pre-trained model...') 
       await shell(`cd ${tmpDir} && ${downloader} https://github.com/Common-Voice/commonvoice-fr/releases/download/v0.6.0-fr-0.3.4/${archiveName}`)
       log.success('Pre-trained model download done')
       log.info('Unpacking...')
       // await shell(`cd ${tmpDir} && tar xvfz ${archiveName}`)
-      await shell(`cd ${tmpDir} && tar -xJf ${archiveName}`)
+      await shell(`cd ${tmpDir} && mkdir models && tar -xJf ${archiveName} && ./models`)
       log.success('Unpack done')
-      log.info('Moving...')
-      await shell(`mv -f ${tmpDir}/deepspeech-${deepSpeechVersion}-models/* ${destDeepSpeechFolder} && rm -rf ${tmpDir}/${archiveName} ${tmpDir}/models`)
+      log.info('Moving...') 
+      await shell(`mv -f ${tmpDir}/models/* ${destDeepSpeechFolder} && rm -rf ${tmpDir}/${archiveName} ${tmpDir}/models`)
       log.success('Move done')
       log.success('Offline speech-to-text installed')
 
